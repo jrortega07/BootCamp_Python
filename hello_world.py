30c50d8fb598ade0fba8c03f76963ab9937d5623
jrortega07 <55067718+jrortega07@users.noreply.github.com>
2021-06-04 19:40:14 -0400

    Add files via upload

--- hello_world.py
+++ hello_world.py
@@ -7,9 +7,14 @@ print("Hola " + name )	# con un +
 # 3. imprimir "Hola 42!" con un numero en una variable
 name = 42
 print( "Hola", name )	# con una coma
-print( "Hola" + name )	# con un + - !Este debería darnos un error!
+print( "Hola " + str(name) )	# con un + - !Este debería darnos un error!
 # 4. imprimir "Me encanta comer sushi and pizza." con los alimentos en variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
 print( "Me encanta comer {} y {}" .format(fave_food1, fave_food2) ) # con .format()
 print( f"Me encanta comer {fave_food1} y {fave_food2}" ) # con una cadena f
+
+food_one = "pasta"
+food_two = "asado"
+print("Me encanta comer {} y {}" .format(food_one, food_two))
+print(f"Me encanta comer {food_one} y {food_two}")
